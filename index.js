b182ee0521072a335c7e23d195a952ba145521d8
Dennis-Kipkogei <kipkogeidennis05@gmail.com>
2025-06-04 11:56:01 +0300

    fix: updated variable values to pass tests

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
-let mostProfitableNeighborhood = 'sixtyfour';
-let companyCeo = 'Chelilim';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
